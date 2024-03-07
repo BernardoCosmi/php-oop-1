9df655c13956daafff242183bf39b5535d51d708
BernardoCosmi <bernardo.daddo90@gmail.com>
2024-03-07 12:58:38 +0100

    feat: print on page

--- index.php
+++ index.php
@@ -16,13 +16,14 @@
 
         // Ottenimento delle informazioni sul film
         public function getInfo() {
-            return "Title: " . $this->title . ", Director: " . $this->director . ", Release Year: " . $this->releaseYear;
+            return "<strong>Director:</strong> ". $this->director. "<br><strong>Release Year:</strong> ".$this->releaseYear;
         }
     }
 
     // Istanziamento di due oggetti Movie
-    $movie1 = new Movie("The Shawshank Redemption", "Frank Darabont", 1994);
-    $movie2 = new Movie("The Godfather", "Francis Ford Coppola", 1972);
+    $movie1 = new Movie("The Lord of the Rings: The Fellowship of the Ring", "Peter Jackson", 2001);
+    $movie2 = new Movie("The Lord of the Rings: The Two Towers", "Peter Jackson", 2002);
+    $movie3 = new Movie("The Lord of the Rings: The Return of the King", "Peter Jackson", 2003);
 ?>
 
 <!DOCTYPE html>
@@ -33,6 +34,33 @@
     <title>php-oop-1</title>
 </head>
 <body>
+    <!-- Stampa a schermo dei risultati -->
+    <h1>
+        Movies
+    </h1>
 
+    <!-- Film 1 -->
+    <h2>
+        <?= $movie1->title?>
+    </h2>
+    <p>
+        <?= $movie1->getInfo()?>
+    </p>
+
+    <!-- Film 2 -->
+    <h2>
+        <?= $movie2->title?>
+    </h2>
+    <p>
+        <?= $movie2->getInfo()?>
+    </p>
+
+    <!-- Film 3 con metodo Echo -->
+    <h2>
+        <?php echo $movie2->title; ?>
+    </h2>
+    <p>
+        <?php echo $movie2->getInfo(); ?>
+    </p>
 </body>
 </html>
