66b50e6d22027c9ac84d16c1a5ccedcaebbdd35e
BernardoCosmi <bernardo.daddo90@gmail.com>
2024-03-07 12:49:33 +0100

    feat: movie class implementation

--- index.php
+++ index.php
@@ -1,5 +1,28 @@
 <?php
+    // Definizione della classe Movie
+    class Movie {
 
+        // Variabili d'istanza
+        public $title;
+        public $director;
+        public $releaseYear;
+
+        // Costruttore
+        public function __construct($title, $director, $releaseYear) {
+            $this->title = $title;
+            $this->director = $director;
+            $this->releaseYear = $releaseYear;
+        }
+
+        // Ottenimento delle informazioni sul film
+        public function getInfo() {
+            return "Title: " . $this->title . ", Director: " . $this->director . ", Release Year: " . $this->releaseYear;
+        }
+    }
+
+    // Istanziamento di due oggetti Movie
+    $movie1 = new Movie("The Shawshank Redemption", "Frank Darabont", 1994);
+    $movie2 = new Movie("The Godfather", "Francis Ford Coppola", 1972);
 ?>
 
 <!DOCTYPE html>
